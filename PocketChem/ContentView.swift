e99dee38b1e6dfc0c66019636985be6e0df417b9
Ajay Gandecha <ajgandecha@gmail.com>
2024-09-03 22:52:01 -0400

    Initial Commit

---
+++ PocketChem/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PocketChem
+//
+//  Created by Ajay Gandecha on 9/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PocketChem/PocketChemApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketChemApp.swift
+//  PocketChem
+//
+//  Created by Ajay Gandecha on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct PocketChemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
